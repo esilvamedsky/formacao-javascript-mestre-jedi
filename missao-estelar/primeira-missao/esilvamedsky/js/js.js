39ce03e00b394dfdefb67f92a49950fbf32f9f5a
esilva <esilva@medsky.pt>
2018-07-20 17:46:41 +0100

    A minha primeira missao estelar dos jedis

---
+++ missao-estelar/primeira-missao/esilvamedsky/js/js.js
@@ -0,0 +1,9 @@
+var missao = 'Missao Estelar JS em execução!!!';
+
+function eventoOnClick(){
+    console.log('Missao Estelar JS realizada!!!');
+}
+
+function eventoOnLoad(){
+    console.log('Evento onload disparado');
+}
